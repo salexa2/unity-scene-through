1e9fe5bf07a2370cd4a2fd0085589f9cc91d0ef1
salexa2 <124205990+salexa2@users.noreply.github.com>
2024-04-16 00:00:56 -0400

    annimation

---
+++ unity-scene-through/Assets/CutsceneController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CutsceneController : MonoBehaviour
+{
+   // Reference to the VideoPlayer component
+  
+   public VideoPlayer finalCutscenePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalCutscenePlayer.loopPointReached += OnFinalCutsceneEnd;
+    
+
+    }
+      
+
+    // Triggered when the final cutscene ends
+    void OnFinalCutsceneEnd(VideoPlayer vp)
+    {
+        // Unsubscribe from the event to prevent multiple calls
+        finalCutscenePlayer.loopPointReached -= OnFinalCutsceneEnd;
+
+        // Load the main menu scene
+        SceneManager.LoadScene("MainMenu");
+    }
+}
