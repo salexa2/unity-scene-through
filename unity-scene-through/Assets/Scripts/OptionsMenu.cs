fc566f7132f62634695dc79b6af881a4789accce
Kayspeedy <KJJordan2000@gmail.com>
2024-04-16 23:08:55 -0400

    Level 3 Stairs
    THEY WORK!!!!

--- unity-scene-through/Assets/Scripts/OptionsMenu.cs
+++ unity-scene-through/Assets/Scripts/OptionsMenu.cs
@@ -7,9 +7,26 @@ public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+    }
+
     public void setVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
         Debug.Log("Volume Working!");
+    }
+
+    public void setQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
