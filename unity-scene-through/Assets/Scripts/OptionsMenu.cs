881eb273371fd82cc435b127b5291d5ba70ba2d3
Kayspeedy <KJJordan2000@gmail.com>
2024-04-18 15:12:30 -0400

    Level 2 and Main
    Level 2 camera shifted a bit.
    Disabled a pesky conveyor belt object
    Almost completed Main Menu scripting.

--- unity-scene-through/Assets/Scripts/OptionsMenu.cs
+++ unity-scene-through/Assets/Scripts/OptionsMenu.cs
@@ -2,16 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+//using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    //public Dropdown resolutionDropdown;
+    public TMP_Dropdown resolutionDropdown;
+
     Resolution[] resolutions;
 
     void Start()
     {
+        //gather all the resolutions possible for the PC we are currently on.
         resolutions = Screen.resolutions;
+
+
+        resolutionDropdown.ClearOptions();
+
+        //List<string> options = new List<string>();
+        TMP_Dropdown.OptionDataList options = new TMP_Dropdown.OptionDataList();
+
+        // Loop through each resolution and add it to the options list
+        /*for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+
+            options.Add(option);
+        }*/
+
+        foreach (Resolution resolution in resolutions)
+        {
+            string optionText = $"{resolution.width} x {resolution.height} @{resolution.refreshRate}Hz";
+            TMP_Dropdown.OptionData option = new TMP_Dropdown.OptionData(optionText);
+            options.options.Add(option);
+        }
+
+        //resolutionDropdown.AddOptions(options);
+        // Assign the list of resolution options to the dropdown
+        resolutionDropdown.options = options.options;
     }
 
     public void setVolume (float volume)
